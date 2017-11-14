c0b212fdc3d25dfa713c4bd6eb395ea97277bbc3
Hunt <hunt.toss@gmail.com>
2017-11-13 19:58:18 -0800

    Updated clean_up.sh script to delete SQS and EC2 instances as well.

--- scripts/clean_up.sh
+++ scripts/clean_up.sh
@@ -2,8 +2,18 @@
 
 set -x
 
-# Variables
-lb_name=loadbalancer-1
+# Make the user confirm
+echo ""
+#read YESNO
+#if [["$YESNO" != "y"]]; then
+    #exit 0
+#fi
+read -p "Are you TOTALLY SURE you want to delete all EC2, RDS, LBs, and SQS instances? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
 
 # Delete load-balancers
 lb_arns=$(aws elbv2 describe-load-balancers --query 'LoadBalancers[*].LoadBalancerArn' --output text)
@@ -22,3 +32,13 @@ do
 done
 
 # Delete EC2 instances
+ec2_ids=$(aws ec2 describe-instances --query 'Reservations[0].Instances[*].InstanceId' --output text)
+aws ec2 terminate-instances --instance-ids ${ec2_ids}
+
+# Delete SQS queues
+queue_urls=$(aws sqs list-queues --query "QueueUrls" --output text)
+for i in ${queue_urls}
+do
+    echo "Deleting Queue: $i"
+    aws sqs delete-queue --queue-url ${i}
+done
